b7ab0f71ccc5a1eeeecae8ec0260b9b09c7b77cd
Yadav3sr <santosh.yadav@3sr-grenoble.fr>
2022-02-04 20:05:36 -0600

    changed made

--- venv/chapter9.py
+++ venv/chapter9.py
@@ -12,6 +12,8 @@ imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces =faceCascade.detectMultiScale(imgGray,1.1,4)
 
+#santosh modified here
+
 for (x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
